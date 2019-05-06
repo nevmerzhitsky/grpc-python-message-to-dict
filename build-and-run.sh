7cb876cd8282d61501433e486fc291420e26389f
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2019-05-06 17:34:40 +0400

    More noticeable build of the image

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 image_name="grpc-python-message-to-dict"
+echo Building image...
 docker build -q -t ${image_name} .
+echo Running container...
 docker run --rm ${image_name}
+echo Done!
