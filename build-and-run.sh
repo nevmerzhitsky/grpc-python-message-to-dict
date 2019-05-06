e49bbb8ae6680413d59be53ef74ea2625989bb83
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2019-05-06 17:23:32 +0400

    Build silent

--- build-and-run.sh
+++ build-and-run.sh
@@ -3,7 +3,7 @@ docker rm -f gpm2d-client gpm2d-server
 
 docker network create grpc-python-message-to-dict
 
-docker build -t grpc-python-message-to-dict .
+docker build -q -t grpc-python-message-to-dict .
 
 common_command="docker run -d --network grpc-python-message-to-dict"
 ${common_command} --name gpm2d-server grpc-python-message-to-dict
